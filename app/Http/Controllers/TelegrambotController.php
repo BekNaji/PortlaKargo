f8a9c6de1ff68e19db0e62315d824022272cdfab
BekNaji <BekNaji@users.noreply.github.com>
2020-07-18 23:30:17 +0300

    Telegram bot updated

--- app/Http/Controllers/TelegrambotController.php
+++ app/Http/Controllers/TelegrambotController.php
@@ -8,14 +8,14 @@ class TelegrambotController extends Controller
     public function index()
     {
         $telegram = new Api('1327273177:AAGsQR9gbP3bzOs0wRmknzGXcsPxmP_U9wY');
-        $response = $telegram::getUpdates();
+        $response = $telegram->getUpdates();
         $lastMessage = end($response);
 
         $chatId = $lastMessage->message->chat->id;
         $lastMessageText = $lastMessage->message->text;
          
         
-        $sendMessage = $telegram::sendMessage([
+        $sendMessage = $telegram->sendMessage([
             'chat_id' => $chatId, 
             'text' => $lastMessageText
         ]);
