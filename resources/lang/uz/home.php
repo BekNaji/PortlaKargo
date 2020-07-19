b38cee9a54bedbd4eafbcad7d77ed38f28151c3a
BekNaji <BekNaji@users.noreply.github.com>
2020-07-19 23:39:05 +0300

    Updated

---
+++ resources/lang/uz/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    
+'home'          => 'Bosh sahifa',
+'about_us'      => 'Biz Kimmiz',
+'contact'       => 'Aloqa',
+'price' 		=> 'Narxlar',
+'login'         => 'Kirish',
+'cargo_number'  => 'Kargo taqim raqami',
+'admin_panel'   => 'Admin Panel',
+'search'        => 'Qidirish',
+'register'        => 'Qayd qilish',
+
+
+];
