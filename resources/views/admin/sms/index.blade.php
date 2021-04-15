a6e2f38285382fff7dd2acebf9a888a9e470c2f6
BekNaji <BekNaji@users.noreply.github.com>
2021-04-15 08:23:31 +0500

    Her sey duzeltildi v1.0 (fixed)
    This reverts commit c787168b7a39302982bc97dd35c2ee4972dea46c.

--- resources/views/admin/sms/index.blade.php
+++ resources/views/admin/sms/index.blade.php
@@ -21,7 +21,7 @@
 				<form action="{{route('sms.update')}}" method="POST">
 					@csrf
 					<div class="form-group">
-						<label>Login</label>
+						<label>Başlık</label>
 						<input value="{{$company->sms_title}}" type="text" name="sms_title" class="form-control">
 					</div>
 					<div class="form-group">
